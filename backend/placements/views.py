77674fc4e6592ca5e3688082631c0f4531a8ac5d
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 01:28:14 +0300

    added placements views

--- backend/placements/views.py
+++ backend/placements/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Placement
+from .serializers import PlacementSerializer
+from rest_framework.permissions import IsAuthenticated
+class PlacementViewSet(viewsets.ModelViewSet):
+    queryset = Placement.objects.all()
+    serializer_class = PlacementSerializer
+    permission_classes = [IsAuthenticated]
