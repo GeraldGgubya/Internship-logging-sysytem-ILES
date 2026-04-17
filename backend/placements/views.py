37faeeef448f8281b11ca509cc97ebfaae139968
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 02:36:58 +0300

    filtered placements

--- backend/placements/views.py
+++ backend/placements/views.py
@@ -9,3 +9,12 @@ class PlacementViewSet(viewsets.ModelViewSet):
     queryset = Placement.objects.all()
     serializer_class = PlacementSerializer
     permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'student':
+            return Placement.objects.filter(student=user)
+        elif user.role == 'supervisor':
+            return Placement.objects.all()
+        else:
+            return Placement.objects.none()
