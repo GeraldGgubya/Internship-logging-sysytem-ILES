0a3a98851ccb0d33268c44c93469489be4eb68d3
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 02:42:43 +0300

    filtered evaluations

--- backend/evaluations/views.py
+++ backend/evaluations/views.py
@@ -9,3 +9,12 @@ class EvaluationViewSet(viewsets.ModelViewSet):
     queryset = Evaluation.objects.all()
     serializer_class = EvaluationSerializer
     permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'student':
+            return Evaluation.objects.filter(placement__student=user)
+        elif user.role == 'supervisor':
+            return Evaluation.objects.all()
+        else:
+            return Evaluation.objects.none()
