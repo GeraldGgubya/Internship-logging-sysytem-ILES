e87ff7399ec1b520af25ee65062dfa8199ca85a6
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 02:29:54 +0300

    filtered weeklylog by role

--- backend/weeklylogs/views.py
+++ backend/weeklylogs/views.py
@@ -9,3 +9,16 @@ class WeeklyLogViewSet(viewsets.ModelViewSet):
     queryset = WeeklyLog.objects.all()
     serializer_class = WeeklyLogSerializer
     permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        # student sees only their logs
+        if user.role == 'student':
+            return WeeklyLog.objects.filter(student=user)
+        # Supervisor sees all logs
+        elif user.role == 'supervisor':
+            return WeeklyLog.objects.all()
+        return WeeklyLog.objects.none()
+    def perform_create(self, serializer):
+        # Automatically assign logged-in student
+        serializer.save(student=self.request.user)
