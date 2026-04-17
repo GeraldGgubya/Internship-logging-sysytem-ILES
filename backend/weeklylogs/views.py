628c80226de4f7edc6b33e3f1f9e011dc587e094
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 01:31:18 +0300

    added weeklylog views

--- backend/weeklylogs/views.py
+++ backend/weeklylogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import WeeklyLog
+from .serializers import WeeklyLogSerializer
+from rest_framework.permissions import IsAuthenticated
+class WeeklyLogViewSet(viewsets.ModelViewSet):
+    queryset = WeeklyLog.objects.all()
+    serializer_class = WeeklyLogSerializer
+    permission_classes = [IsAuthenticated]
