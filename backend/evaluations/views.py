9d5923f4feed0c7156b5035c0732b865ea9c45ac
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 01:33:46 +0300

    added evaluations views

--- backend/evaluations/views.py
+++ backend/evaluations/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Evaluation
+from .serializers import EvaluationSerializer
+from rest_framework.permissions import IsAuthenticated
+class EvaluationViewSet(viewsets.ModelViewSet):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerializer
+    permission_classes = [IsAuthenticated]
