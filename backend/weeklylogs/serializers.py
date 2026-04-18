6076763aada73481c26755b0004da2ca2167d645
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 03:01:58 +0300

    modidified weeklylog serializers

--- backend/weeklylogs/serializers.py
+++ backend/weeklylogs/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import WeeklyLog
 class WeeklyLogSerializer(serializers.ModelSerializer):
+    student_username = serializers.CharField(source='student.username', read_only=True)
     class Meta:
         model = WeeklyLog
         fields = '__all__'
