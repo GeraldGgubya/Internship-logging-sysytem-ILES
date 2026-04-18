b0d49b13aabdf69642bc44cb68098e9934aca21a
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 03:05:15 +0300

    modified placement serializers

--- backend/placements/serializers.py
+++ backend/placements/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Placement
 class PlacementSerializer(serializers.ModelSerializer):
+    student_username = serializers.CharField(source='student.username', read_only=True)
     class Meta:
         model = Placement
         fields = '__all__'
