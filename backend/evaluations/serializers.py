84912d82dcb96fd71410b97dd556c62ce6e432e6
GeraldGgubya <geraldggubya@gmail.com>
2026-04-18 03:08:33 +0300

    modified evaluations serializers

--- backend/evaluations/serializers.py
+++ backend/evaluations/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Evaluation
 class EvaluationSerializer(serializers.ModelSerializer):
+    student_username = serializers.CharField(source='student.username', read_only=True)
     class Meta:
         model = Evaluation
         fields = '__all__'
