eeea7c57070a99b40d97cbd171bb516055d36620
GeraldGgubya <geraldggubya@gmail.com>
2026-09-04 12:54:39 +0300

    Fix AllowAny import

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from weeklylogs.models import WeeklyLog
 from placements.models import Placement
